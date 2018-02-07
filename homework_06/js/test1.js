4639ac8c93e8a20c1b648602af4d9ff140d1b36b
oleksandr <oleksandr1991xperia@gmail.com>
2018-02-07 16:50:27 +0200

    jjj

--- homework_06/js/test1.js
+++ homework_06/js/test1.js
@@ -8,15 +8,28 @@ if ((a <= 0) | (b <= 0) | (c <= 0)) {
 	console.log('For data ' + a + ', ' + b + ', ' + c + ':' + ' Incorrect data');
 } else if ((a == b) & (b == c) & (c == a)) {
 	s1 = (a*a*Math.sqrt(3))/4;
-	console.log('Type of triangle is equilateral triangle and square is ' + s1); // equilateral triangle
+	var f = s1.toFixed(2);
+	console.log('For data ' + a + ', ' + b + ', ' + c + ':');
+	console.log('"Type of triangle is equilateral triangle and square is "'  + f ); // equilateral triangle
 } else if (a == c) {
 	s2 = b*(Math.sqrt(a*a - (b*b/4)))/2;
-	console.log('Type of triangle is isosceles triangle and square is ' + s2); // isosceles triangle
+	console.log('Type of triangle is isosceles triangle and square is ' + s2.toFixed(2)); // isosceles triangle
 } else if (b == c) {
 	s2 = a*(Math.sqrt(b*b - (a*a/4)))/2;
-	console.log('Type of triangle is isosceles triangle and square is ' + s2); // isosceles triangle
+	console.log('Type of triangle is isosceles triangle and square is ' + s2.toFixed(2)); // isosceles triangle
 } else if (a == b) {
 	s2 = c*(Math.sqrt(a*a - (c*c/4)))/2;
-	console.log('Type of triangle is isosceles triangle and square is ' + s2); // isosceles triangle
+	console.log('Type of triangle is isosceles triangle and square is ' + s2.toFixed(2)); // isosceles triangle
+} else if (a*a == b*b + c*c){
+	s2 = b*c/2;
+	console.log('Type of triangle is right triangle and square is ' + s2.toFixed(2)); // right triangle
+} else if (b*b == a*a + c*c){
+	s2 = a*c/2;
+	console.log('Type of triangle is right triangle and square is ' + s2.toFixed(2)); // right triangle
+} else if (c*c == a*a + b*b){
+	s2 = a*b/2;
+	console.log('Type of triangle is right triangle and square is ' + s2.toFixed(2)); // right triangle
 }
+
+
 
